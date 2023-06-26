46a344882c3291b7cbbcb34126979596006bea8d
Rahul Jaideep <rahul.jaideep@ibm.com>
2023-06-26 13:10:29 -0400

    most game logic works

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
 'use strict';
+
+let curr_player = 0;
+
+function random_start_player() {
+    if (Math.random() < 0.5) {
+        curr_player = 0;
+    } else {
+        curr_player = 1;
+    }
+    document.querySelector(`.player--${curr_player}`).classList.add('player--active');
+    document.querySelector(`.player--${1 - curr_player}`).classList.remove('player--active');
+}
+
+function reset_game() {
+    document.querySelector('.dice').src = `images/dice-5.png`;
+    document.getElementById('score--0').textContent = 0;
+    document.getElementById('score--1').textContent = 0;
+    document.getElementById('current--0').textContent = 0;
+    document.getElementById('current--1').textContent = 0;
+    random_start_player();
+}
+
+function next_player() {
+    curr_player = 1 - curr_player;
+    document.querySelector(`.player--${curr_player}`).classList.add('player--active');
+    document.querySelector(`.player--${1 - curr_player}`).classList.remove('player--active');
+
+}
+
+function generate_num() {
+    return Math.ceil(Math.random() * 6);
+}
+
+function display_dice(num) {
+    document.querySelector('.dice').src = `images/dice-${num}.png`;
+}
+
+function roll_handler() {
+    let num = generate_num();
+    display_dice(num);
+    if (num == 1) {
+        document.getElementById(`score--${curr_player}`).textContent = 0;
+        document.getElementById(`current--${curr_player}`).textContent = 0;
+        next_player();
+    } else {
+        let new_score = Number(document.getElementById(`score--${curr_player}`).textContent) + num;
+        document.getElementById(`score--${curr_player}`).textContent = new_score;
+        // if (new_score > 100) {
+        //     ...
+        // }
+    }
+}
+
+function hold_handler() {
+    let before_score = Number(document.getElementById(`current--${curr_player}`).textContent);
+    let increase_score = Number(document.getElementById(`score--${curr_player}`).textContent);
+    let new_score = before_score + increase_score;
+    document.getElementById(`current--${curr_player}`).textContent = new_score;
+    document.getElementById(`score--${curr_player}`).textContent = 0;
+    next_player()
+}
+
+document.querySelector('.btn--hold').addEventListener('click', hold_handler);
+
+document.querySelector('.btn--roll').addEventListener('click', roll_handler);
+
+document.querySelector('.btn--new').addEventListener('click', reset_game);
